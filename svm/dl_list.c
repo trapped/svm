2b9efa60763115b9d7a75b4479fc87097e20cb52
trapped <giorgio@pellero.it>
2015-04-19 15:48:22 +0200

    Fix dl_list

--- svm/dl_list.c
+++ svm/dl_list.c
@@ -1,6 +1,19 @@
 #include "dl_list.h"
 
 void dl_push(dl_list** l, void* value) {
-  dl_list new = { .value = value, .prev = 0x0, .next = *l };
-  *l = &new;
+  dl_list* new = calloc(1, sizeof(dl_list));
+  dl_list* first;
+  if(!*l) {
+    first = new;
+  } else {
+    first = (*l)->first ? (*l)->first : *l;
+  }
+  new->value = value;
+  new->first = first;
+  new->prev  = *l;
+  if(*l) {
+    (*l)->next = new;
+  }
+  new->next  = NULL;
+  *l = new;
 }
--- svm/dl_list.h
+++ svm/dl_list.h
@@ -1,8 +1,11 @@
 #ifndef DL_LIST_H
 #define DL_LIST_H
 
+#include <stdlib.h>
+
 typedef struct dl_list {
   void* value;
+  struct dl_list* first;
   struct dl_list* prev;
   struct dl_list* next;
 } dl_list;
